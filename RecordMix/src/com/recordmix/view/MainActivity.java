15d53cc0a9975fcf8ce9c2635d170238a66f5821
wuzhengyu <saky721@163.com>
2014-07-16 10:49:33 +0800

    change logo

--- RecordMix/src/com/recordmix/view/MainActivity.java
+++ RecordMix/src/com/recordmix/view/MainActivity.java
@@ -73,7 +73,6 @@ public class MainActivity extends Activity implements OnClickListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		mContext = MainActivity.this;
